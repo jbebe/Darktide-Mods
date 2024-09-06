3544f2b388e384c133a6c93187ca55617a02025e
Bálint Juhász <jbebe@users.noreply.github.com>
2024-09-07 00:55:37 +0200

    not used, due to the discovery of scratchpad

--- lovesmenot/scripts/mods/lovesmenot/mod.commands.lua
+++ lovesmenot/scripts/mods/lovesmenot/mod.commands.lua
@@ -1,28 +1,5 @@
 local mod = get_mod("lovesmenot")
-local json = mod:io_dofile("lovesmenot/scripts/mods/lovesmenot/thirdparty/json")
 local UIRenderer = mod:original_require("scripts/managers/ui/ui_renderer")
-
-mod:command("lmn_cmd", "Run simple functions (reset)", function(functionName)
-    if functionName == "reset" then
-        mod.initialized = true
-        mod.localPlayer = Managers.player:local_player_safe(1)
-        mod:loadRating()
-        mod.isInMission = mod:_isInMission()
-    end
-end)
-
-mod:command("lmn_get", "Get property on local player object", function(modProperty, subProperty)
-    local value = mod[modProperty]
-    if subProperty then
-        value = value[subProperty]
-    end
-
-    mod:echo(json.encode(value))
-end)
-
-mod:command("lmn_save", "Save state to file", function()
-    mod:persistRating()
-end)
 
 mod:command("lmn_debug", "", function()
     mod.debugging = not mod.debugging
